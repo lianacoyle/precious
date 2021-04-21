acc67452bd618a395cf4c76720be4e5468a8d172
Liana Coyle <lianacoyle@gmail.com>
2021-04-20 18:09:44 -0700

    combined Liana's files and shop page

--- app.py
+++ app.py
@@ -1,155 +1,89 @@
 #run in cmd terminal: 
-# python -m pip install flask-sqlalchemy
-# python -m pip install pymysql
-# python -m pip install Pillow
+# python -m pip install {library name}
 
 
-from flask import Flask, render_template, request, redirect 
-from flask_sqlalchemy import SQLAlchemy
-#import pymysql
-#from PIL import image 
+from flask import Flask, render_template, request, redirect, flash, url_for
 import os
 
+from werkzeug.utils import secure_filename
+
+
+class Item:
+    def __init__(self, photo_name, price, file_name, category=None, item_desc=None):
+        self.item_name = photo_name
+        self.category = category
+        self.item_desc = item_desc
+        self.price = price
+        self.file_name = file_name
+        self.tn_file_name = None
+
+
+#creating an empty list for Photo objects
+items = []
+
+#instantiating Photo objects and adding them to list of photos
+pic1 = Item("Paper Lantern Alley", 10.00, 'pic1.jpg', '', "Some interesting description here")
+items.append(pic1)
+pic2 = Item("Rainy Street Glimmer", 15.00, 'pic2.jpg')
+items.append(pic2)
+pic3 = Item("Tax Free Night Sign", 12.00, 'pic3.jpg')
+items.append(pic3)
+pic4 = Item("Baby Pandas", 11.00, 'pic4.jpg')
+items.append(pic4)
+pic5 = Item("Pink Sky Mountain", 16.00, 'pic5.jpg')
+items.append(pic5)
+
+
 app = Flask(__name__)
-
-#Database attributes for connection
-# will move credentials to separate config file later 
-conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format('root', '','localhost','precious')
-app.config['SQLALCHEMY_DATABASE_URI'] = conn 
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']
-
-#SQLAlchemy class definitions for each table using ORM (Object Relational Mapping)
-#Columns defined here MUST match have matching attributes to database columns
-
-class User(db.Model):
-    iduser = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(45), nullable=False)
-    last_name = db.Column(db.String(45), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    vendor_flag = db.Column(db.Boolean, default=False)
-    create_time = db.Column(db.DateTime, nullable=True)
-
-#Example of instantiating a user object/record and adding it to the database
-user1 = User(iduser=1001, first_name='Some', last_name='One', email='mail', password='password', vendor_flag=True)
-#db.session.add(user1)
-#db.session.commit()
-
-'''
-class item(db.Model):
-    iditem = db.Column(db.Integer, primary_key=True)
-    image = db.Column(db.LargeBinary)
-    item_name = db.Column(db.String(100), nullable=False)
-    idcategory = db.Column(db.Integer, db.ForeignKey('category.idcategory'), nullable=False)
-    item_desc = db.Column(db.String(255), nullable=False)
-    price = db.Column(db.Numeric(7,2), nullable=False)
-    create_time = db.Column(db.DateTime, nullable=True)
-
-def convertToBinary(filename):
-    with open(filename, 'rb') as file:
-        binarydata=file.read()
-    return binarydata
-
-def convertBinaryToFile(binarydata, filename):
-    with open(filename, 'wb') as file:
-        file.write(binarydata)
-
-    
-
-class cart(db.Model):
-    idcart = db.Column(db.Integer, primary_key=True)
-    create_time = db.Column(db.DateTime, nullable=True)
-    update_time = db.Column(db.DateTime, nullable=True)
-    iduser = db.Column(db.DateTime, db.ForeignKey('user.iduser'), nullable=True)
-
-
-class item(db.Model):
-    idcartdetails = db.Column(db.Integer, primary_key=True)
-    quantity = db.Column(db.Integer, nullable=False)
-    extended_price = db.Column(db.Numeric(7,2), nullable=False)
-    idcart = db.Column(db.Integer, db.ForeignKey('cart.idcart'), nullable=False)
-    iditem = db.Column(db.Integer, db.ForeignKey('item.iditem'), nullable=False)
-
-
-class category(db.Model):
-    idcategory = db.Column(db.Integer, primary_key=True)    
-    category_name = db.Column(db.String(100), nullable=False)
-    category_desc = db.Column(db.String(255), nullable=False)
-'''
-
-
-
-
-
-#
 
 #root/home page
 @app.route("/")
 @app.route("/precious")
 def precious():
-	return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/form')
 def form():
     return render_template('form.html')
-'''
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
-    if request.method == 'GET':
-        return "Login via the login form"
 
-    if request.method == 'POST':
-        first_name = request.form['First Name']
-        last_name = request.form['Last Name']
-        email = request.form['Email']
-        password = request.form['Password']
-        confirmPassword = request.form['Confirm Password']
-        #Creating a connection cursor
-        cursor = db.connection.cursor()
-        cursor.execute( INSERT INTO seller_table VALUES(%s, %s,%s,%s), (first_name, last_name, email, password))
-        db.connection.commit()
-        cursor.close()
-        return f"Done!!"
-'''
 @app.route("/phot")
 def phot():
-	return "Photography"
+    return "Photography"
 
 @app.route("/contact")
 def contact():
-	return render_template('Contactus.html')
+    return render_template('Contactus.html')
 
 @app.route("/me")
 def me():
-	return "PM"
+    return "PM"
 
 
 @app.route("/des")
 def designs():
-	return "Des"
-	
-@app.route("/shop")
+    return "Des"
+
+@app.route('/shopping')
 def shopping():
-	return "shop"
-	
+    return render_template('shopping.html', items=items)
+
 @app.route("/profile")
 def profile():
-	return "profile"
+    return "profile"
 
 @app.route("/login")
 def login():
-	return render_template('login.html')
+    return render_template('login.html')
 
 @app.route("/logout")
 def logout():
-	return "You have completed a successful logout!"
+    return "You have completed a successful logout!"
 
 # user checkout page
 @app.route("/ucos")
 @app.route("/userCheckout")
 def uco():
-	return render_template('userCheckout.html')
+    return render_template('userCheckout.html')
 
 
 # user upload page
@@ -161,7 +95,7 @@ def allowed_file(filename):
 @app.route("/uua") 
 @app.route('/userUpload')
 def upload_form():
-	return render_template('userUpload.html')
+    return render_template('userUpload.html')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload():
@@ -194,11 +128,9 @@ def upload():
 @app.route("/scn") 
 @app.route('/shoppingCart')
 def upload_cart():
-	return render_template('shoppingCart.html')
+    return render_template('shoppingCart.html')
           
 if __name__ == '__main__':
-		app.run(debug=True)
+    app.run(debug=True)
 
-app.run(host='localhost', port= 5000)
-
-#just adding this comment to show a change on git
+app.run(host='localhost', port=5000)
